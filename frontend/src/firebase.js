4284165625faf94bc8106313a30af82f28c0e66f
ARPIT SHARMA <arpitsharma8975@gmail.com>
2025-09-03 21:03:04 +0530

    modify

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAKtOOeNBhAWn29RQuZH2QLna4IMg9wPw",
+  authDomain: "ayurahaar-5decc.firebaseapp.com",
+  projectId: "ayurahaar-5decc",
+  storageBucket: "ayurahaar-5decc.firebasestorage.app",
+  messagingSenderId: "125707190802",
+  appId: "1:125707190802:web:f62bb5c204bd710d3bcecb"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
